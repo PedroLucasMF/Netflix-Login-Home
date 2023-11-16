2ecdf40d1f00b7393ceebe4eea7ccead37d04c4c
PedroMF <pedrolucas3049@gmail.com>
2023-11-16 20:46:15 -0300

    login feito

---
+++ README.md
@@ -0,0 +1,4 @@
+# Netflix Login & Home Page
+---
+
+This repository is a simple try to make a login adn home page of Netflix in react Native using the MovieDB
---
+++ screens/Login.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { ScrollView, View } from 'react-native'
+import { Button, Card, Text, TextInput } from 'react-native-paper'
+import { StyleSheet } from 'react-native-web'
+
+const Login = (Navigator) => {
+
+  
+  return (
+    <>
+      <ScrollView>
+        <View style={styles.tela}>
+          <Text style={styles.redLogo}>Netflix</Text>
+          <TextInput
+            style={styles.forms}
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.forms}
+            placeholder="Password"
+            secureTextEntry
+          />
+          <Button style={styles.buttao}><Text style={styles.bigBlue}>Entrar</Text></Button>
+
+
+
+
+
+
+        </View>
+      </ScrollView>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: '#696868',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  redLogo: {
+    color: 'red',
+    fontSize: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 30
+  },
+  forms: {
+    margin: 20,
+    marginBottom: 0,
+    marginTop: 5,
+    backgroundColor: '#ddd',
+    color: '#9c9c9c',
+    backgroundColor: '#373738'
+  },
+  tela: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#000',
+    height: 605,
+  },
+  buttao: {
+    marginTop: 10,
+    borderWidth: 2,
+    borderColor: '#5c5c5c', // Grey border color
+    borderRadius: 1,
+    paddingHorizontal: 80,
+    paddingVertical: 2,
+    backgroundColor: '#373738',
+  }
+});
+
+export default Login
